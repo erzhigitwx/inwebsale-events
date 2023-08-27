6e54ba646a4d7bf8aae338f579c86645353faa8e
erzhigitwx <110293971+erzhigitwx@users.noreply.github.com>
2023-08-27 12:52:58 +0600

    add output

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "localhost", "inwebsale-events-prod.vercel.app"],
-  }
+  },
+  output: "export"
 };
 
 module.exports = nextConfig;
