b34609b6705d77ff351a71eefde2903069473226
erzhigitwx <110293971+erzhigitwx@users.noreply.github.com>
2023-08-28 21:41:41 +0600

    env

--- src/app/_config/auth.ts
+++ src/app/_config/auth.ts
@@ -2,6 +2,7 @@ import type { AuthOptions } from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const authConfig: AuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     Google({ clientId: process.env.CLIENT_ID!, clientSecret: process.env.CLIENT_SECURE! })
   ],
